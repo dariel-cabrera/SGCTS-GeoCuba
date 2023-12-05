f1f1301b4026e5bc5389ad317138326ebb4045fe
Iglu <125487939+dariel-cabrera@users.noreply.github.com>
2023-12-04 23:05:09 -0500

    Creando Tabla de Interfaz

--- SGCTS/cliente/interfaz.py
+++ SGCTS/cliente/interfaz.py
@@ -1,5 +1,5 @@
 import tkinter as tk
-
+from tkinter  import ttk
 
 #Barra de Menu Archivo Inicio Funcionalidades MiPerfil
 def barra_menu(root):
@@ -36,6 +36,7 @@ class Frame(tk.Frame):
         
         self.campo_variables()
         self.desabilitar_campos()
+        self.tabla_listado_calculos()
         
     
     
@@ -89,7 +90,7 @@ class Frame(tk.Frame):
         self.label_angulo.config(font= ('Arial',12,'bold'),bg='#0299e4')
         # Posicion de los Label
         # para separar un los label se usa padx y pady
-        self.label_angulo.grid(row=1,column=3, padx=10,pady=10)
+        self.label_angulo.grid(row=1,column=2, padx=10,pady=10)
 
          # Label Índice de Rompiente
         self.label_indice= tk.Label(self,text= 'Índice de Rompiente (k) : ')
@@ -97,7 +98,7 @@ class Frame(tk.Frame):
         self.label_indice.config(font= ('Arial',12,'bold'),bg='#0299e4')
         # Posicion de los Label
         # para separar un los label se usa padx y pady
-        self.label_indice.grid(row=2,column=3, padx=10,pady=10)
+        self.label_indice.grid(row=2,column=2, padx=10,pady=10)
 
          # Label Acelaración Gravitacional
         self.label_aceleracion= tk.Label(self,text= 'Acelaración Gravitacional (g) : ')
@@ -105,51 +106,51 @@ class Frame(tk.Frame):
         self.label_aceleracion.config(font= ('Arial',12,'bold'),bg='#0299e4')
         # Posicion de los Label
         # para separar un los label se usa padx y pady
-        self.label_aceleracion.grid(row=3,column=3, padx=10,pady=10)
+        self.label_aceleracion.grid(row=3,column=2, padx=10,pady=10)
 
         # Entrys de cada campo
         # Entrys Densidad_Mar
         
         self.mi_densidad_mar= tk.StringVar()
         self.entry_densidad_mar= tk.Entry(self,textvariable= self.mi_densidad_mar)
-        self.entry_densidad_mar.config(width=10, font=('Arial',12))
-        self.entry_densidad_mar.grid(row=1, column=1,padx=10,pady=10,columnspan= 2)
+        self.entry_densidad_mar.config(width=20, font=('Arial',12))
+        self.entry_densidad_mar.grid(row=1, column=1,padx=10,pady=10)
 
          # Entrys Densidad_Arena
         self.mi_densidad_arena= tk.StringVar()
         self.entry_densidad_arena= tk.Entry(self,textvariable= self.mi_densidad_arena)
-        self.entry_densidad_arena.config(width=10, font=('Arial',12))
-        self.entry_densidad_arena.grid(row=2, column=1,padx=10,pady=10,columnspan= 2)
+        self.entry_densidad_arena.config(width=20, font=('Arial',12))
+        self.entry_densidad_arena.grid(row=2, column=1,padx=10,pady=10)
 
         # Entrys coeficiente_porocidad
         self.mi_coeficiente_porocidad= tk.StringVar()
         self.entry_coeficiente_porocidad= tk.Entry(self,textvariable= self.mi_coeficiente_porocidad)
-        self.entry_coeficiente_porocidad.config(width=10, font=('Arial',12))
-        self.entry_coeficiente_porocidad.grid(row=3, column=1,padx=10,pady=10,columnspan= 2)
+        self.entry_coeficiente_porocidad.config(width=20, font=('Arial',12))
+        self.entry_coeficiente_porocidad.grid(row=3, column=1,padx=10,pady=10)
 
          # Entrys Altura
         self.mi_altura= tk.StringVar()
         self.entry_altura= tk.Entry(self,textvariable= self.mi_altura)
-        self.entry_altura.config(width=10, font=('Arial',12))
-        self.entry_altura.grid(row=4, column=1,padx=10,pady=10,columnspan= 2)
+        self.entry_altura.config(width=20, font=('Arial',12))
+        self.entry_altura.grid(row=4, column=1,padx=10,pady=10)
 
         # Entrys angulo_rompiente
         self.mi_angulo_rompiente= tk.StringVar()
         self.entry_angulo_rompiente= tk.Entry(self,textvariable= self.mi_angulo_rompiente)
-        self.entry_angulo_rompiente.config(width=10, font=('Arial',12))
-        self.entry_angulo_rompiente.grid(row=1, column=4,padx=10,pady=10,columnspan= 2)
+        self.entry_angulo_rompiente.config(width=20, font=('Arial',12))
+        self.entry_angulo_rompiente.grid(row=1, column=3,padx=10,pady=10)
 
          # Entrys indice_rompiente
         self.mi_indice_rompiente= tk.StringVar()
         self.entry_indice_rompiente= tk.Entry(self,textvariable= self.mi_indice_rompiente)
-        self.entry_indice_rompiente.config(width=10, font=('Arial',12))
-        self.entry_indice_rompiente.grid(row=2, column=4,padx=10,pady=10,columnspan= 2)
+        self.entry_indice_rompiente.config(width=20, font=('Arial',12))
+        self.entry_indice_rompiente.grid(row=2, column=3,padx=10,pady=10)
 
         # Entrys acelaración_gravitacional
         self.mi_acelaración_gravitacional= tk.StringVar()
         self.entry_acelaración_gravitacional= tk.Entry(self,textvariable= self.mi_acelaración_gravitacional)
-        self.entry_acelaración_gravitacional.config(width=10, font=('Arial',12))
-        self.entry_acelaración_gravitacional.grid(row=3, column=4,padx=10,pady=10,columnspan= 2)
+        self.entry_acelaración_gravitacional.config(width=20, font=('Arial',12))
+        self.entry_acelaración_gravitacional.grid(row=3, column=3,padx=10,pady=10)
 
         #Boton Nuevo 
         self.boton_nuevo= tk.Button(self,text="Nuevo",command =self.habilitar_campos)
@@ -159,17 +160,17 @@ class Frame(tk.Frame):
         # cursor='hand2' cambiar el curso de flecha a manito
         # activebackground - Para cambiar el color cuando le de al boton 
         font=('Arial',12,'bold'),fg='white',bg='#0299e4',cursor='hand2',activebackground= 'gray')
-        self.boton_nuevo.grid(row=5, column=2,padx=10,pady=10)
+        self.boton_nuevo.grid(row=5, column=1,padx=10,pady=10)
         
         # Boton Procesar
-        self.boton_procesar= tk.Button(self,text="Procesar",command = self.guardar_datos)
+        self.boton_procesar= tk.Button(self,text="Procesar",command= self.guardar_datos)
         self.boton_procesar.config(width=15,
         # en htmlcolorcodes.com se obtienen los colores 
         # fg- color de letra bg - color de fondo
         # cursor='hand2' cambiar el curso de flecha a manito
         # activebackground - Para cambiar el color cuando le de al boton 
         font=('Arial',12,'bold'),fg='white',bg='#0299e4',cursor='hand2',activebackground= 'gray')
-        self.boton_procesar.grid(row=5, column=3,padx=10,pady=10)
+        self.boton_procesar.grid(row=5, column=2,padx=10,pady=10)
 
          # Boton Cancelar
         self.boton_cancelar= tk.Button(self,text="Cancelar",command = self.desabilitar_campos)
@@ -179,7 +180,7 @@ class Frame(tk.Frame):
         # cursor='hand2' cambiar el curso de flecha a manito
         # activebackground - Para cambiar el color cuando le de al boton 
         font=('Arial',12,'bold'),fg='red',bg='white',cursor='hand2',activebackground= 'gray')
-        self.boton_cancelar.grid(row=5, column=4,padx=10,pady=10)
+        self.boton_cancelar.grid(row=5, column=3,padx=10,pady=10)
 
          # Metodos para habilitar los campos
     # La funcion se activa cuando le dan al boton a nuevo
@@ -226,9 +227,42 @@ class Frame(tk.Frame):
         self.boton_procesar.config(state='disabled')
         self.boton_cancelar.config(state='disabled')
 
-     def guardar_datos(self):
+    def guardar_datos(self):
+        self.desabilitar_campos()
+
+    def tabla_listado_calculos(self): 
+        #Recuperar la lista de peliculas
+        #self.lista_peliculas= listar()
+        #self.lista_peliculas.reverse() 
         
-        self.desabilitar_campos
+        self.tabla= ttk.Treeview (self, column=('Ubicacion','ρ', 'ρs','n','Hb','α','k','g','K','Q'))
+        self.tabla.config(bg='blue')
+        self.tabla.grid(row=6,column=0,columnspan= 11)
+
+        #Scrollbar para la tabla si excede 10 registros
+        #self.scroll=ttk.Scrollbar(self,orient='vertical',command=self.tabla.ysiew)
+        #self.scroll.grid(row=6,column=4,sticky='nse')
+        #self.tabla.configure(yscrollcommand= self.scroll.set)
+
+
+        self.tabla.heading('#0',text='ID')
+        self.tabla.heading('#1',text='UBICACION')
+        self.tabla.heading('#2',text='ρ')
+        self.tabla.heading('#3',text='ρs')
+        self.tabla.heading('#4',text='n')
+        self.tabla.heading('#5',text='Hb')
+        self.tabla.heading('#6',text='α')
+        self.tabla.heading('#7',text='k')
+        self.tabla.heading('#8',text='g')
+        self.tabla.heading('#9',text='K')
+        self.tabla.heading('#10',text='Q')
+        
+        
+
+        #Iterar la lista de peliculas
+        #for p in self.lista_peliculas:
+            #self.tabla.insert('', o, text= p[0])
+            #values=(p[1],p[2],p[3])
 
     
  
